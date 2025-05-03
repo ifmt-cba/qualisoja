926d3729da7b01e2a80f6308dcb4267bec261aa9
Mafe519 <fernandacamposcavalcante@hotmail.com>
2025-05-02 23:27:43 -0400

    Alterações na interface

--- analises/urls.py
+++ analises/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from analises import views
 
 urlpatterns = [
-    path('cadastrarAnalise/', views.cadastrarAnalise, name="CadastrarAnalise")
+    path('cadastrarAnalise/', views.cadastrarAnalise, name="CadastrarAnalise"),
+    path('salvarAnalise/', views.cadastrarAnalise, name="salvarAnalise")
 ]
--- analises/views.py
+++ analises/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+#def cadastrarAnalise(request):
+#    return render(request, 'cadastrarAnalise.html')
+
+# views.py
 def cadastrarAnalise(request):
-    return render(request, 'cadastrarAnalise.html')
+    contexto = {
+        'intervalo': range(1, 9),  # Gera 1 até 8
+    }
+    return render(request, 'cadastrarAnalise.html', contexto)
