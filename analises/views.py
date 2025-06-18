507646f2fa3e5fc21fce3c6f844ab98abe0dcc79
ogeorgehenrique <engenheiro.georgehenrique@gmail.com>
2025-06-18 14:09:31 -0400

    Implementar o Calculo da analise de Umidade do Óleo degomado (#64)

--- analises/views.py
+++ analises/views.py
@@ -25,6 +25,32 @@ class OleoDegomadoCreateView(CreateView):
     template_name = 'app/cadastro_oleo.html'
     success_url = reverse_lazy('analises:oleo_list')
 
+    def form_valid(self, form):
+        titulacao = form.cleaned_data['titulacao']
+        fator_correcao = form.cleaned_data['fator_correcao']
+        peso_amostra = form.cleaned_data['peso_amostra']
+        tipo_analise = form.cleaned_data['tipo_analise']
+        tara = form.cleaned_data['tara']
+        liquido = form.cleaned_data['liquido']
+        
+        if tipo_analise == 'UMI':
+            if tara and fator_correcao and peso_amostra:
+                resultado = ((tara + peso_amostra) - (liquido))/peso_amostra*100
+                form.instance.resultado = resultado.quantize(Decimal('0.01'))  # arredonda para 2 casas decimais
+
+        if tipo_analise == 'ACI':
+            if titulacao and fator_correcao and peso_amostra:
+                # Use Decimal para valores numéricos literais
+                resultado = (titulacao * fator_correcao * Decimal('28.2') * Decimal('100')) / peso_amostra
+                form.instance.resultado = resultado.quantize(Decimal('0.01'))  # arredonda para 2 casas decimais
+
+        if tipo_analise == 'SAB':
+            if titulacao and fator_correcao and peso_amostra:
+                resultado = (titulacao * fator_correcao * Decimal('300.4') * Decimal('100')) / peso_amostra
+                form.instance.resultado = resultado.quantize(Decimal('0.01'))  # arredonda para 2 casas decimais
+        return super().form_valid(form)
+
+
 class UmidadeListView(ListView):
     model = AnaliseUmidade
     template_name = 'app/lista_umidade.html'
