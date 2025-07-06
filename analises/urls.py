bd83453d632d6a787598606792f17d4181d9fde9
Rodrigo Henriky <65931987+rhenriky@users.noreply.github.com>
2025-07-06 01:11:54 -0300

    Anlise Urase

--- analises/urls.py
+++ analises/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .views import (
     AnaliseHomeView,
-    UmidadeCreateView, ProteinaCreateView, OleoDegomadoCreateView,
-    UmidadeListView, ProteinaListView, OleoDegomadoListView
+    UmidadeCreateView, ProteinaCreateView, OleoDegomadoCreateView,UraseCreateView,
+    UraseListView,UmidadeListView, ProteinaListView, OleoDegomadoListView
 )
 
 app_name = 'analises'
@@ -15,4 +15,6 @@ urlpatterns = [
     path('umidade/', UmidadeListView.as_view(), name='umidade_list'),
     path('proteina/', ProteinaListView.as_view(), name='proteina_list'),
     path('oleo/', OleoDegomadoListView.as_view(), name='oleo_list'),
+    path('urase/cadastro/', UraseCreateView.as_view(), name='urase_create'),
+    path('urase/', UraseListView.as_view(), name='urase_list'),
 ]
